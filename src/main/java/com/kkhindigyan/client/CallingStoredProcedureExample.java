238ed45265340442de81e0057c200099c87774c2
kkhindigyan01 <kkhindigyan01@gmail.com>
2022-04-01 18:33:12 +0530

    Get ResultSet By Calling StoredProcedure Using CallableStatement Example

--- src/main/java/com/kkhindigyan/client/CallingStoredProcedureExample.java
+++ src/main/java/com/kkhindigyan/client/CallingStoredProcedureExample.java
@@ -1,10 +1,44 @@
 package com.kkhindigyan.client;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Types;
+import java.util.Scanner;
+
+import com.kkhindigyan.util.DBUtil;
+
 public class CallingStoredProcedureExample {
 
 	public static void main(String[] args) {
-
 		
+		String CallStoreProc = "CALL getEmployeeNameAndSalaryById(?,?,?)";
+		try(Connection conn = DBUtil.getMySQLConnection();
+				CallableStatement callableStatement = conn.prepareCall(CallStoreProc);
+				Scanner scanner = new Scanner(System.in) ) {
+			
+			System.out.println("Enter Employee Id:");
+			
+			int empId = scanner.nextInt();
+			
+			callableStatement.setInt(1, empId);
+			
+			callableStatement.registerOutParameter(2, Types.VARCHAR);
+			callableStatement.registerOutParameter(3, Types.DOUBLE);
+			
+			//callableStatement.execute();
+			
+			int result = callableStatement.executeUpdate();
+			
+			if(result >0) {
+				System.out.println("Employee Id ="+empId);
+				System.out.println("Employee Name ="+callableStatement.getString("empName"));
+				System.out.println("Employee Salary  ="+callableStatement.getDouble("empSal"));
+			}else {
+				System.out.println("Employee with Id ="+empId+" not found in database");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
---
+++ src/main/java/com/kkhindigyan/client/GetResultSetByCallingStoredProcedureExmaple.java
@@ -0,0 +1,41 @@
+package com.kkhindigyan.client;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Types;
+import java.util.Scanner;
+
+import com.kkhindigyan.util.DBUtil;
+
+/**
+ * Get ResultSet By Calling StoredProcedure Using CallableStatement
+ * @author KK HindiGyan
+ *
+ */
+public class GetResultSetByCallingStoredProcedureExmaple {
+
+	public static void main(String[] args) {
+	
+		String CallStoreProc = "CALL getAllEmployees";
+		
+		try(Connection conn = DBUtil.getMySQLConnection();
+				CallableStatement callableStatement = conn.prepareCall(CallStoreProc);
+				ResultSet rs = callableStatement.executeQuery()) {
+			
+			while (rs.next()) {
+				System.out.println("Employee Id ="+rs.getInt("employee_id"));
+				System.out.println("Employee Name ="+rs.getString("employee_name"));
+				System.out.println("Employee Salary  ="+rs.getDouble("employee_salary"));
+				System.out.println("Employee Dept = "+rs.getString("employee_dept"));
+				System.out.println("Employee DOJ = "+rs.getDate("employee_doj"));
+				System.out.println("----------------------------------------");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	}
+
